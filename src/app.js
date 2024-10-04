0abdb55654ad9fe45e4f50c95fa0b20db58ab3ea
Nepta <63590772+Neptali-Ricardo@users.noreply.github.com>
2024-10-04 15:13:28 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -9,3 +9,35 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+const getRandom = array => Math.floor(Math.random() * array.length);
+
+const excuseGenerator = () => {
+  return `
+    <strong>${who[getRandom(who)]}</strong>
+    ${action[getRandom(action)]}
+    <strong>${what[getRandom(what)]}</strong>
+    ${when[getRandom(when)]}
+  `;
+};
+
+const excuseText = () => {
+  const excuseElement = document.getElementById("excuse-generator__text");
+  if (excuseElement) {
+    excuseElement.innerHTML = excuseGenerator();
+  }
+};
+
+excuseText();
+setInterval(excuseText, 2500);
